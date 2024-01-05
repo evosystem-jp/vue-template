33dc2683fe590566cd61c3b3e5e530774f6da6ff
cyrus07424 <cyrus07424@users.noreply.github.com>
2024-01-05 04:49:27 +0000

    deploy: b3006e79ed451ec5c4652cd513b8c703c6d93255

---
+++ precache-manifest.57b8d1c55d9d519ec81f39e23cf071ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f6030a8536cb15b80e2",
+    "url": "js/about.js"
+  },
+  {
+    "revision": "c4d0d4504ca491227fa3",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "c4d0d4504ca491227fa3",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "cd2e31cfda0f4bcb28ff",
+    "url": "js/chunk-vendors.js"
+  },
+  {
+    "revision": "c1b42b1fb1a57381ec33aaebd74e4735",
+    "url": "img/logo.png"
+  },
+  {
+    "revision": "bd5473370074dc8ae904f162a428b3cd",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
